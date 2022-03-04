4d6e4a6eaa2835d3942661fedac69a3fb68e4fb3
Ryugi <rsuguro@gmail.com>
2022-03-05 00:11:59 +0900

    Added Projects model with sample templates

---
+++ projects/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+def projects(request):
+  context = {}
+
+  return render(request, 'projects/projects.html', context)
+
+
+def project(request, pk):
+  context = {}
+
+  return render(request, 'projects/single-project.html', context)
