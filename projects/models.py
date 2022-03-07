17c96a76c721e4368a926b5ccda0829cc0353914
Ryugi <rsuguro@gmail.com>
2022-03-07 22:13:22 +0900

    Added MEDIA_* & STATICFILES configuration with additional features in models|forms|views to handle user-uploaded images(static)

--- projects/models.py
+++ projects/models.py
@@ -6,6 +6,7 @@ class Project(models.Model):
   id = models.UUIDField(default=uuid.uuid4, primary_key=True, unique=True, editable=False)
   title = models.CharField(max_length=200)
   description = models.TextField(null=True, blank=True)
+  featured_image = models.ImageField(default="default.jpg", null=True, blank=True)
   demo_link = models.CharField(max_length=2000, null=True, blank=True)
   source_link = models.CharField(max_length=2000, null=True, blank=True)
   tags = models.ManyToManyField('Tag', blank=True)
