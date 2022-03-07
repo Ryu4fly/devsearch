e3e3a357529ef9cace16357d3f8e284e565c0b5a
Ryugi <rsuguro@gmail.com>
2022-03-08 00:42:23 +0900

    Implemented themes for Project | Projects | Forms

--- projects/forms.py
+++ projects/forms.py
@@ -1,4 +1,5 @@
 from django.forms import ModelForm
+from django import forms
 from .models import Project
 
 
@@ -6,3 +7,15 @@ class ProjectForm(ModelForm):
   class Meta:
     model = Project
     exclude = ['vote_ratio', 'vote_total']
+    widgets = {
+      'tags': forms.CheckboxSelectMultiple()
+    }
+
+  def __init__(self, *args, **kwargs):
+    super(ProjectForm, self).__init__(*args, **kwargs)
+
+    for name, field in self.fields.items():
+      field.widget.attrs.update({'class': 'input', 'placeholder': 'Add Title'})
+
+    # Can edit each individual field
+    # self.fields['title'].widget.attrs.update({'class': 'input', 'placeholder': 'Add Title'})
