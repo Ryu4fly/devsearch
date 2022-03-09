121f00ce6a2d2b2654d6a18bad51898a748fc297
Ryugi <rsuguro@gmail.com>
2022-03-09 11:32:54 +0900

    Added user profile/s themes

--- users/models.py
+++ users/models.py
@@ -10,6 +10,7 @@ class Profile(models.Model):
   name = models.CharField(max_length=200, null=True, blank=True)
   username = models.CharField(max_length=200, null=True, blank=True)
   email = models.EmailField(max_length=500, null=True, blank=True, unique=True)
+  location = models.CharField(max_length=200, null=True, blank=True)
   short_intro = models.CharField(max_length=200, null=True, blank=True)
   bio = models.TextField(null=True, blank=True)
   profile_image = models.ImageField(upload_to='profiles/', default='profiles/user-default.png', null=True, blank=True)
@@ -23,3 +24,11 @@ class Profile(models.Model):
     return self.user.username
 
 
+class Skill(models.Model):
+  id = models.UUIDField(default=uuid.uuid4, primary_key=True,unique=True, editable=False)
+  owner = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True, blank=True)
+  name = models.CharField(max_length=200, null=True, blank=True)
+  description = models.TextField(null=True, blank=True)
+
+  def __str__(self):
+    return self.name
