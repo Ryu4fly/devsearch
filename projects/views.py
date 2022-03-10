8e3846157577618077d58cd59d606d80cc99c162
Ryugi <rsuguro@gmail.com>
2022-03-10 10:20:51 +0900

    Added User Authentication & Authorization

--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render, redirect
 from .models import Project
 from .forms import ProjectForm
+
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+
 
 def projects(request):
   projects = Project.objects.all()
@@ -20,6 +23,7 @@ def project(request, pk):
   return render(request, 'projects/single-project.html', context)
 
 
+@login_required(login_url='login')
 def createProject(request):
   form = ProjectForm()
 
@@ -35,6 +39,7 @@ def createProject(request):
   return render(request, 'projects/project_form.html', context)
 
 
+@login_required(login_url='login')
 def updateProject(request, pk):
   project = Project.objects.get(id=pk)
   form = ProjectForm(instance=project)
@@ -51,6 +56,7 @@ def updateProject(request, pk):
   return render(request, 'projects/project_form.html', context)
 
 
+@login_required(login_url='login')
 def deleteProject(request, pk):
   project = Project.objects.get(id=pk)
   context = {'object': project}
