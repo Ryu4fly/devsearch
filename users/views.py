53cae7b31452dc99f977f384d63d35b55c9db1ec
Ryugi <rsuguro@gmail.com>
2022-03-10 18:46:04 +0900

    added user registration

--- users/views.py
+++ users/views.py
@@ -1,10 +1,10 @@
-from .models import Profile
-
 from django.contrib import messages
-from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
 
+from .forms import CustomUserCreationForm
+from .models import Profile
 
 def loginUser(request):
   if request.user.is_authenticated:
@@ -36,6 +36,26 @@ def logoutUser(request):
 
   return redirect('login')
 
+def registerUser(request):
+  page = 'register'
+
+  form = CustomUserCreationForm()
+
+  if request.method == 'POST':
+    form = CustomUserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save(commit=False)
+      user.username = user.username.lower()
+      user.save()
+      messages.success(request, 'User Created')
+      login(request, user)
+      return redirect('profiles')
+    else:
+      messages.error(request, 'An error has occurred')
+
+  context = {'page': page, 'form': form}
+
+  return render(request, 'users/login_register.html', context)
 
 def profiles(request):
   profiles = Profile.objects.all()
