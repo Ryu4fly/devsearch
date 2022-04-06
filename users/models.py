af862d2d2f7c955a4d23767037884d4da543f7cd
Ryugi <rsuguro@gmail.com>
2022-04-06 23:13:34 +0900

    Removed turtle and matpotlib

--- users/models.py
+++ users/models.py
@@ -3,10 +3,6 @@ import uuid
 from django.db import models
 from django.contrib.auth.models import User
 
-import matplotlib
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
-
 class Profile(models.Model):
   id = models.UUIDField(default=uuid.uuid4, primary_key=True, unique=True, editable=False)
   user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
